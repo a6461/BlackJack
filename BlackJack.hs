cfe411ae85d64ff46a36a79cf196d1bb62d35511
Alexander Vasilenko <a6461@yandex.ru>
2015-12-12 21:47:01 +0300

    Добавлена генерация случайной колоды

--- BlackJack.hs
+++ BlackJack.hs
@@ -25,9 +25,11 @@ scoreCard :: Card -> Bool -> Int
 scoreCard (Card Ace _) False = 11 
 scoreCard (Card v _) _ = min (fromEnum v + 1) 10
 
+-- Очки "руки" 
 scoreHand :: Hand -> Int 
 scoreHand h = fst $ foldl (\(s,b) c -> if (not b && v c == Ace) then (s + (scoreCard c False), True) else (s + (scoreCard c b), b)) (0,False) h
 
+-- Случайная карта
 randomCard :: IO Card
 randomCard = do
   g <- newStdGen
@@ -36,3 +38,13 @@ randomCard = do
   let s = fst $ randomR (0, 3) g
   let c = Card ((toEnum v) :: Value) ((toEnum s) :: Suit)
   return $ Card ((toEnum v) :: Value) ((toEnum s) :: Suit)
+
+-- Случайная колода (от 0 до 52 карт)
+shuffleDeck :: Int -> IO Deck
+shuffleDeck n
+  | (n >= 0 && n < 53) = shuffleDeck' n []
+  | otherwise = error "The number of cards in a range [0..52]"
+  where
+   shuffleDeck' :: Int -> [Card] -> IO Deck 
+   shuffleDeck' 0 cs = return cs
+   shuffleDeck' i cs = randomCard >>= (\c -> if (elem c cs) then (shuffleDeck' i cs) else (shuffleDeck' (i-1) (c:cs)))
